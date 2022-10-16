37248f2a7e2d2d8d4baf59d43d4576bbadbbf886
Piotr Fabiszewski <piotr.fabiszewski@gmail.com>
2022-10-16 13:25:47 +0200

    fix: displaying links in header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,24 @@
 import Link from 'next/link';
 
+const routes = [
+  { href: '/', name: 'Home' },
+  { href: '/about', name: 'About' },
+];
+
 export const Header = () => {
   return (
     <header>
       <nav>
         <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
+          {routes.map((route) => {
+            return (
+              <li key={route.href}>
+                <Link href={route.href}>
+                  <a>{route.name}</a>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
